2912a8d8731d88ea535c09bb0ad655852875783b
Grouter <matus.perfekt@gmail.com>
2020-08-29 16:54:49 +0200

    [Add] Bullet destroy

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
@@ -6,18 +6,35 @@ public class Shoot : MonoBehaviour
     public Vector2 direction;
     public float speed;
     public GameObject source;
+    public int life = 2;
+
+    private void Start() 
+    {
+        Invoke("DestroyBullet", 2);
+    }
 
     private void Update()
     {
-        this.transform.position += new Vector3(direction.x + (speed * Time.deltaTime), direction.y + (speed * Time.deltaTime), 0.0f);
+        this.transform.position += new Vector3(direction.x * (speed * Time.deltaTime), direction.y * (speed * Time.deltaTime), 0.0f);
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.tag == "Player" && other.gameObject != source) 
+        if (other.gameObject == source)
+            return;
+
+        if (other.tag == "Player") 
         {
             Player player = other.GetComponent<Player>();
             player.Hit(1);
         }
+
+        DestroyBullet();
+    }
+    
+    private void DestroyBullet() 
+    {
+        Debug.Log("ASDASD");
+        Destroy(gameObject);
     }
 }
